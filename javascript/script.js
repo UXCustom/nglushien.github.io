8286aee010881fda871855e53f61124bf62bb9f5
Noah Glushien <nglushien@techtarget.com>
2014-06-03 22:31:53 -0400

    modified many things

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,44 @@
 $(document).ready(function(){
 
-	$(window).on('resize',function(){
-		if($(window).width() < 640){
-			$('i').attr('class','fa fa-mobile-phone');
-			$('.experience').text("You are viewing the mobile experience");
-		} else if($(window).width() < 960){
-			$('i').attr('class','fa fa-tablet');
-			$('.experience').text("You are viewing the tablet experience");
-		} else if($(window).width() < 1280){
-			$('i').attr('class','fa fa-laptop');
-			$('.experience').text("You are viewing the laptop experience");
-		} else {
-			$('i').attr('class','fa fa-desktop');
-			$('.experience').text("You are viewing the desktop experience");
-		}
+	// $(window).on('resize',function(){
+	// 	if($(window).width() < 640){
+	// 		$('i').attr('class','fa fa-mobile-phone');
+	// 		$('.experience').text("You are viewing the mobile experience");
+	// 	} else if($(window).width() < 960){
+	// 		$('i').attr('class','fa fa-tablet');
+	// 		$('.experience').text("You are viewing the tablet experience");
+	// 	} else if($(window).width() < 1280){
+	// 		$('i').attr('class','fa fa-laptop');
+	// 		$('.experience').text("You are viewing the laptop experience");
+	// 	} else {
+	// 		$('i').attr('class','fa fa-desktop');
+	// 		$('.experience').text("You are viewing the desktop experience");
+	// 	}
+	// });
+
+	// if($(window).width() < 640){
+	// 	$('i').attr('class','fa fa-mobile-phone');
+	// 	$('.experience').text("You are viewing the mobile experience");
+	// } else if($(window).width() < 960){
+	// 	$('i').attr('class','fa fa-tablet');
+	// 	$('.experience').text("You are viewing the tablet experience");
+	// } else if($(window).width() < 1280){
+	// 	$('i').attr('class','fa fa-laptop');
+	// 	$('.experience').text("You are viewing the laptop experience");
+	// } else {
+	// 	$('i').attr('class','fa fa-desktop');
+	// 	$('.experience').text("You are viewing the desktop experience");
+	// }
+
+document.querySelector( "#nav-toggle" )
+	.addEventListener( "click", function() {
+		this.classList.toggle( "active" );
 	});
 
-	if($(window).width() < 640){
-		$('i').attr('class','fa fa-mobile-phone');
-		$('.experience').text("You are viewing the mobile experience");
-	} else if($(window).width() < 960){
-		$('i').attr('class','fa fa-tablet');
-		$('.experience').text("You are viewing the tablet experience");
-	} else if($(window).width() < 1280){
-		$('i').attr('class','fa fa-laptop');
-		$('.experience').text("You are viewing the laptop experience");
-	} else {
-		$('i').attr('class','fa fa-desktop');
-		$('.experience').text("You are viewing the desktop experience");
-	}
+	$('#nav-toggle').on('click',function(){
+		$('.container').toggleClass('shifted');
+		$('.mobileMenu').toggle();
+	}); 
 
 
 });
