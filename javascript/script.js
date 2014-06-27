5c96759920d43b68a22697e5da97f0f0ba881054
Noah Glushien <nglushien@techtarget.com>
2014-06-27 15:25:32 -0400

    Updated many files

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,51 @@
 $(document).ready(function(){
 
-	$('#nav-toggle').on('click',function(){
-		$('.container').toggleClass('shiftContainer');
-		$('.mobileMenu').toggleClass('shiftMenu');
-		$(this).toggleClass('active position');
+	$('.navigationContainer a[href^="#"]').on('click', function(event) {
+		var target = $( $(this).attr('href') );
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top,
+			}, 1500, 'easeInOutCubic');
+		}
+	});
+	
+
+	if($(window).scrollTop() <= 940 ){
+		$('.navigationContainer .home').addClass('active');
+		$('.navigationContainer a').not('.home').removeClass('active');
+	} else if($(window).scrollTop() > 940 && $(window).scrollTop() < 1880){
+		$('.navigationContainer .design').addClass('active');
+		$('.navigationContainer a').not('.design').removeClass('active');
+	} else if($(window).scrollTop() > 1880 && $(window).scrollTop() < 2820){
+		$('.navigationContainer .dev').addClass('active');
+		$('.navigationContainer a').not('.dev').removeClass('active');
+	}
+
+	$(window).on('scroll',function(){
+		if($(window).scrollTop() <= 940 ){
+			$('.navigationContainer .home').addClass('active');
+			$('.navigationContainer a').not('.home').removeClass('active');
+		} else if($(window).scrollTop() > 940 && $(window).scrollTop() < 1880){
+			$('.navigationContainer .design').addClass('active');
+			$('.navigationContainer a').not('.design').removeClass('active');
+		} else if($(window).scrollTop() > 1880 && $(window).scrollTop() < 2820){
+			$('.navigationContainer .dev').addClass('active');
+			$('.navigationContainer a').not('.dev').removeClass('active');
+		}
 	});
 
-	var menuPosition = function() {
-
-		var winHeight = $(window).height();
-		$('#nav-toggle').css({'position':'fixed','left':'-111px','top':winHeight / 2 - ($('#nav-toggle').height() / 2)});
-
-	};
-
-	menuPosition();
+	$('.intro').addClass('hidden').viewportChecker({
+		classToAdd: 'visible animated fadeIn',
+		offset: 100
+	});
+	$('#design,#dev').children().addClass('hidden');
+	setTimeout(function(){
+		$('#design,#dev').children().viewportChecker({
+			classToAdd: 'visible animated fadeInDown',
+			offset: 200
+		});
+	},3000);
 
 
 });
